b0d105ec1f761ef1da3447a7c33c2a2c35de447f
Charith <rajithacharith@gmail.com>
2018-11-19 09:28:11 +0530

    login done and student dashboard staterd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    StudentDashboardComponent
   ],
   imports: [
     BrowserModule
